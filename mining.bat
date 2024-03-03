f8c33b55643205215fcb5189e62a347b1ba060b3
snapslap <162002879+snapslap@users.noreply.github.com>
2024-03-03 09:08:24 +0000

    Add files via upload

---
+++ mining.bat
@@ -0,0 +1,2 @@
+xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u USDT:0x62b24931ee1f085e7d3229829a3b091d6704e16f.Usdt1 -p x
+pause 
---
+++ start (1).cmd
@@ -0,0 +1,3 @@
+@echo off
+xmrig.exe -o rx.unmineable.com:3333 -u USDT:0x8c5224209d89c42a6db43e8bcf57128b6a75d64a.WorkerUsdt#53gw-odg5 -p user -k -a rx/0
+pause
